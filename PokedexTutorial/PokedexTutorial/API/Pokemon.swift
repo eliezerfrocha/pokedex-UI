5f6d1c7f2d8127d9ad2d9df20e5721fd62db2f8f
Eliezer <eliezer@MacBook-Pro-de-Eliezer.local>
2024-01-07 14:49:42 -0300

    First commit

---
+++ PokedexTutorial/PokedexTutorial/API/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokedexTutorial
+//
+//  Created by Eliezer   on 07/01/24.
+//
+//https://pokeapi.co/api/v2/pokemon?limit=151
+
+import Foundation
---
+++ PokedexTutorial/PokedexTutorial/PokedexTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTutorialApp.swift
+//  PokedexTutorial
+//
+//  Created by Eliezer   on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
